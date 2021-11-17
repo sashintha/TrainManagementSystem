141cc1be36ef7caa826097efc16f8db843ba20cd
Sashintha Tennakoon <34927075+sashintha@users.noreply.github.com>
2021-11-17 10:07:34 -0500

    created login page

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require('express');
-
 const app = express();
+
+app.use(express.static('static'));
+
+app.use('/',(req,res) =>{
+    
+    res.sendFile(__dirname + '/static/login.html');
+})
+
+
+app.listen(2222);
