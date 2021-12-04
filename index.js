0ec7d7f8c38f41e0bc2da5c063f2a4ad8e4c846b
jmutton2 <jmutton2@uwo.ca>
2021-12-04 16:23:48 -0500

    indexjs commenting

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const newConnection = require('./DBConnection'); //receive connection function
 
 app.use(express.static(__dirname + '/public')); //load styling sheets
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set("view engine","ejs");
+app.set("view engine","ejs"); //Set view engine to EJS
 
 //##GET##//
 app.get('/', (req, res) => {
+  //Redirect to login page
   res.sendFile(__dirname + '/static/login.html');
 });
 
-
+//Main page
 app.get('/dashboard', (req, res) => {
+  //Set connection
   let conn = newConnection();
+  //Run 4 queries 
   var sql = 
   `
   SELECT * FROM schedule; 
@@ -22,73 +25,103 @@ app.get('/dashboard', (req, res) => {
   SELECT * FROM employee;
   SELECT * FROM maintenanceLog WHERE  ${app.get('currentType')} = '${app.get('currentLogin')}';
   `
-  ;
+  
+  //Send the query 
   conn.query(sql, function (err, rows, fields) {
     if (err) 
       throw err
     else{
+      //Render each query
       res.render('dashboard', {scheduleData: rows[0], t_employees: rows[1], employeeTable: rows[2], main_log: rows[3]});
       }
   })
+  //End the connection
   conn.end();
 });
 
+//Scehdule talble
 app.get('/schedule', (req, res) => {
+  //Create a new connection
   let conn = newConnection();
+    //Send the FIRST COMPLEX FUNCTION QUERY
     conn.query(`
     SELECT train.trainNo as TrainNo, train.stationName as StationName, arrivalTime, departureTime, routeStatus
     FROM train
     INNER JOIN schedule
     ON train.trainNo = schedule.trainNo
-    WHERE rdyForUtil = 'No';`
+    WHERE rdyForUtil = 'Yes';`
   , function (err, rows, fields) {
     if (err) 
       throw err
     else{
+      //Render the response
       res.render('schedule', { logs: rows });
       }
   })
+  //End the connection
   conn.end();
 });
 
+//Employee table
 app.get('/employees', (req, res) => {
+  //Create a new connection
   let conn = newConnection();
-    conn.query(`SELECT * FROM employee`
+  //Get all employees
+  conn.query(`SELECT * FROM employee`
   , function (err, rows, fields) {
     if (err) 
       throw err
     else{
+      //Render the response
       res.render('employees', { e_list: rows });
       }
   })
+  //End the connection
   conn.end();
 });
 
+//Maintenance Table
 app.get('/maintenance', (req, res) => {
+  //Start the connection
   let conn = newConnection();
+
+  //Create the SECOND COMPLEX FUNCTION QUERY
+  //Get all the logs correspondig to the logged in user
+  //Create the THIRD COMPLEX FUNCTION QUERY
+  //Get all carts and their maintenances in descending order
   var sql =
   `
   SELECT * FROM maintenanceLog WHERE ${app.get('currentType')} = '${app.get('currentLogin')}';
   SELECT cartNo, COUNT(*) FROM maintenanceLog GROUP BY cartNo
   `
+  //Sen the query
   conn.query(sql, function (err, rows, fields) {
     if (err) 
       throw err
     else{
+      //Render the response
       res.render('maintenance', { main_log: rows[0], cartCount: rows[1]});
       }
   })
+  //End the connection
   conn.end();
 });
 
 
 //##POST##//
 app.post('/', (req, res) => {
+
+  //Set default global variables
   app.set('currentLogin', req.body.email);
   app.set('currentType', 'supervisedBy');
 
+  //Global variable to ensure user exists
   app.set('actuallyExists', false);
+
+  //Create connection and sql statement
   let conn = newConnection();
+  
+  //Return each table if the email exists in the list specified
   var sql =
   `
   SELECT maintainPerformBy
@@ -98,30 +131,43 @@ app.post('/', (req, res) => {
   FROM maintenanceLog
   WHERE EXISTS (SELECT supervisedBy FROM maintenanceLog WHERE supervisedBy = '${app.get('currentLogin')}');
   `
+  //Run the query
   conn.query(sql, function (err, rows, fields) {
+    //Loop through the first query
     for (row in rows[0]) {
+      //If the number of tuples is greater than 10
       if (row > 10) {
+        //Set that the employee exists and the type of employee who is logging in
         app.set('actuallyExists', true);
         app.set('currentType', 'maintainPerformBy');
         break;
       }
     }
+
+    //Loop through the second query
     for (row in rows[1]) {
+      //If the number of tuples is greater than 10
       if (row > 10) {
+        //Set that the employee exists and the type of employee who is logging in
         app.set('actuallyExists', true);
         app.set('currentType', 'supervisedBy');
         break;
       }
     }
 
+    //If the employee doesnt exist redirect to login
     if (app.get('actuallyExists') == false) {
       res.redirect('/');
+    //If teh user exists and types admin as the password, login
     } else if (req.body.password == 'admin') {
       res.redirect('/dashboard');
+    //Otherwise redirect to login
     } else {
       res.redirect('/');
     }
   })
+
+  //End the connection
   conn.end();
 });
 
